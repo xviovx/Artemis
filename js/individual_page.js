472af45164c50fc7ad4bf498609fd2909240892e
xviovx <71275388+xviovx@users.noreply.github.com>
2020-11-11 18:36:23 +0200

    individual movie page functionality complete
    still need to fix the genre code

--- js/individual_page.js
+++ js/individual_page.js
@@ -12,13 +12,58 @@ $(function(){
 
         for(i = 0; i < result.results.length; i++) {
             if(result.results[i].id === pageID){
-                var title;
-                var genre;
-                var rating;
-                var description;
+                var backdrop = result.results[i].backdrop_path;
+                var poster = result.results[i].poster_path;
+                var title = result.results[i].title;
+                var genre = result.results[i].genre_ids;//WORK OUT GENRE BASED ON IDS!!!!!
+                var releaseDate = result.results[i].release_date;
+                var avgVote = result.results[i].vote_average;
+                var description = result.results[i].overview;
+                var ogLang = result.results[i].original_language;
+
+                    var imgSrc = 'https://image.tmdb.org/t/p/w500/';
                 
-            }
-        }
+                $("#backdrop-header").attr("src", imgSrc + backdrop);
+                $("#movie-poster-x").attr("src", imgSrc + poster);
+                $("#header-text").text(title);
+
+                for(i = 0; i < result.results.length; i++) {
+                    if (genre === 28){
+                        genreName = "Action";
+                    } else if (genre === 12){
+                        genreName = "Adventure";
+                    } else if (genre === 16){
+                        genreName = "Animation";
+                    } else if (genre === 35){
+                        genreName = "Comedy";
+                    } else if (genre === 80){
+                        genreName = "Crime";
+                    } else if (genre === 99){
+                        genreName = "Documentary";
+                    } else if (genre === 18){
+                        genreName = "Drama";
+                    }else if (genre === 10751){
+                        genreName = "Family";
+                    } else if (genre === 14){
+                        genreName = "Fantasy";
+                    } else if (genre === 36){
+                        genreName = "History";
+                    } else if (genre === 878){
+                        genreName = "Sci-Fi";
+                    } else {
+                        genreName = "Unknown";
+                    };
+                };
+
+                $("#genre-text").text(genreName);
+                $("#release-date").text(releaseDate);
+                $("#average-vote").text(avgVote);
+                $("#description_text").text(description);
+                $("#og-lang-text").text(ogLang);
+
+            };
+        };
 
     });
 })
+
