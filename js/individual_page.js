fb1dbdab899f461c13ea8151393923d431944470
xviovx <71275388+xviovx@users.noreply.github.com>
2020-11-11 17:01:07 +0200

    changes to individual movie page
    made the necessary changes to fit the API info

--- js/individual_page.js
+++ js/individual_page.js
@@ -1,7 +1,24 @@
 $(function(){
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
-    var pageID = urlParams.get('id');
+    var pageID = parseInt(urlParams.get('id'));
 
     console.log(pageID);
+
+    api_key= "4c72fa62556dd6e032a91d50c5f47265";
+
+    var endPoint = "https://api.themoviedb.org/3/movie/popular?api_key="+ api_key +"&language=en-US&page=1";
+    var data = $.getJSON(endPoint, function(result){
+
+        for(i = 0; i < result.results.length; i++) {
+            if(result.results[i].id === pageID){
+                var title;
+                var genre;
+                var rating;
+                var description;
+                
+            }
+        }
+
+    });
 })
